944131bc78071f31f55c85ba8ae073c687b0c91e
Matthew Nock <thematt.nock@gmail.com>
2024-06-16 18:42:47 +0100

    assign meshs in editor

---
+++ Floor.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Floor : RigidBody3D
+{
+	[Export]
+	private Mesh[] meshs;
+	[Export]
+	private int selected = 0;
+	[Export]
+	private MeshInstance3D meshInstance;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		meshInstance.Mesh = meshs[selected];
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
