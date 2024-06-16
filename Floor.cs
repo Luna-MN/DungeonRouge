bbc60c03ff037cc5be708efc7ccc60c39740db34
Matthew Nock <thematt.nock@gmail.com>
2024-06-16 21:59:53 +0100

    error heneling floor.cs

--- Floor.cs
+++ Floor.cs
@@ -13,6 +13,10 @@ public partial class Floor : RigidBody3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		if (selected < 0 || selected >= meshs.Length)
+		{
+			selected = 0;
+		}
 		meshInstance.Mesh = meshs[selected];
 	}
 
