24ce5804e92fe4496e210c0f12ae19ac73c84c4e
Matthew Nock <thematt.nock@gmail.com>
2024-06-23 14:57:02 +0100

    dash

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,6 +5,8 @@ public partial class Player : RigidBody3D
 {
 	private Key oldkey;
 	private int speed = 20;
+	private Timer timer = new Timer();
+	private bool timerOn = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,103 +15,115 @@ public partial class Player : RigidBody3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print(timer.TimeLeft);
+		if (timer.IsStopped())
+		{
+			speed = 20;
+			timerOn = false;
+		}
 	}
 	public override void _Input(InputEvent @event)
 	{
 		if (@event is InputEventKey keyP)
 		{
-			if (Input.IsKeyPressed(Key.W))
+			if (timerOn == false)
 			{
-				if (keyP.Pressed)
+				if (Input.IsKeyPressed(Key.W))
 				{
-					if (oldkey == Key.S)
+					if (keyP.Pressed)
 					{
-						LinearVelocity *= new Vector3(1, 1, 0);
+						if (oldkey == Key.S)
+						{
+							LinearVelocity *= new Vector3(1, 1, 0);
+						}
+						oldkey = Key.W;
 					}
-					oldkey = Key.W;
-				}
-				if (LinearVelocity.Z <= 1)
-				{
-					LinearVelocity += Vector3.Back * speed;
-				}
-				if (LinearVelocity.Z < 20)
-				{
-					LinearVelocity += Vector3.Back;
-				}
-			}
-			if (Input.IsKeyPressed(Key.S))
-			{
-				if (keyP.Pressed)
-				{
-					if (oldkey == Key.W)
+					if (LinearVelocity.Z <= 1)
 					{
-						LinearVelocity *= new Vector3(1, 1, 0);
+						LinearVelocity += Vector3.Back * speed;
 					}
-					oldkey = Key.S;
-				}
-				if (LinearVelocity.Z >= -1)
-				{
-					LinearVelocity += Vector3.Forward * speed;
-				}
-				if (LinearVelocity.Z > -20)
-				{
-					LinearVelocity += Vector3.Forward;
-				}
-			}
-			if (Input.IsKeyPressed(Key.D))
-			{
-				if (keyP.Pressed)
-				{
-					if (oldkey == Key.A)
+					if (LinearVelocity.Z < 20)
 					{
-						LinearVelocity *= new Vector3(0, 1, 1);
+						LinearVelocity += Vector3.Back;
 					}
-					oldkey = Key.D;
 				}
-				if (LinearVelocity.X >= -1)
+				if (Input.IsKeyPressed(Key.S))
 				{
-					LinearVelocity += Vector3.Left * speed;
-				}
-				if (LinearVelocity.X > -20)
-				{
-					LinearVelocity += Vector3.Left;
-				}
-			}
-			if (Input.IsKeyPressed(Key.A))
-			{
-				GD.Print(LinearVelocity);
-				if (keyP.Pressed)
-				{
-					if (oldkey == Key.D)
+					if (keyP.Pressed)
 					{
-						LinearVelocity *= new Vector3(0, 1, 1);
+						if (oldkey == Key.W)
+						{
+							LinearVelocity *= new Vector3(1, 1, 0);
+						}
+						oldkey = Key.S;
 					}
-					oldkey = Key.A;
+					if (LinearVelocity.Z >= -1)
+					{
+						LinearVelocity += Vector3.Forward * speed;
+					}
+					if (LinearVelocity.Z > -20)
+					{
+						LinearVelocity += Vector3.Forward;
+					}
 				}
-				if (LinearVelocity.X <= 1)
+				if (Input.IsKeyPressed(Key.D))
 				{
-					LinearVelocity += Vector3.Right * speed;
+					if (keyP.Pressed)
+					{
+						if (oldkey == Key.A)
+						{
+							LinearVelocity *= new Vector3(0, 1, 1);
+						}
+						oldkey = Key.D;
+					}
+					if (LinearVelocity.X >= -1)
+					{
+						LinearVelocity += Vector3.Left * speed;
+					}
+					if (LinearVelocity.X > -20)
+					{
+						LinearVelocity += Vector3.Left;
+					}
 				}
-				if (LinearVelocity.X < 20)
+				if (Input.IsKeyPressed(Key.A))
 				{
-					LinearVelocity += Vector3.Right;
+					GD.Print(LinearVelocity);
+					if (keyP.Pressed)
+					{
+						if (oldkey == Key.D)
+						{
+							LinearVelocity *= new Vector3(0, 1, 1);
+						}
+						oldkey = Key.A;
+					}
+					if (LinearVelocity.X <= 1)
+					{
+						LinearVelocity += Vector3.Right * speed;
+					}
+					if (LinearVelocity.X < 20)
+					{
+						LinearVelocity += Vector3.Right;
+					}
 				}
-			}
-			if (!Input.IsKeyPressed(Key.W) && !Input.IsKeyPressed(Key.S))
-			{
-				LinearVelocity *= new Vector3(1, 1, 0);
-			}
-			if (!Input.IsKeyPressed(Key.A) && !Input.IsKeyPressed(Key.D))
-			{
-				LinearVelocity *= new Vector3(0, 1, 1);
+				if (!Input.IsKeyPressed(Key.W) && !Input.IsKeyPressed(Key.S))
+				{
+					LinearVelocity *= new Vector3(1, 1, 0);
+				}
+				if (!Input.IsKeyPressed(Key.A) && !Input.IsKeyPressed(Key.D))
+				{
+					LinearVelocity *= new Vector3(0, 1, 1);
+				}
 			}
 			if (Input.IsKeyPressed(Key.Shift) && keyP.Pressed)
 			{
-				speed *= 2;
-			}
-			if (!Input.IsKeyPressed(Key.Shift) && keyP.Pressed)
-			{
-				speed /= 2;
+				Timer timer = new Timer
+				{
+					Autostart = true,
+					WaitTime = 0.5f,
+					OneShot = true
+				};
+				speed = 40;
+				timerOn = true;
 			}
 		}
 
